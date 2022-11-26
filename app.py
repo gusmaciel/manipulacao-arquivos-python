fa20233d925bf83f0654bf62ec60c23758670dbd
gusmaciel <gtvsmaciel@outlook.com>
2022-11-26 17:37:28 -0300

    commit-update

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('hello world')
+#open("caminho","r")
+
+# Mode
+# r - Leitura
+# a - Append / Incrementar
+# w - Escrita
+# x - Criar arquivo
+# r+ - Leitura + Escrita
+
+arquivo = open('text.txt','w')
+
+arquivo.write('Ola Mundo')
+
+arquivo.close()
+
+print('Hello World')
+
---
+++ tiposprimitivos.py
@@ -0,0 +1,8 @@
+n1=int(input('Digite um número: '))
+n2=int(input('Digite outro número: '))
+
+resultado = n1 + n2
+
+#print('O resultado da soma de',n1,'+',n2,'é: {}'.format(resultado))
+
+print('O resultado da soma de {} + {} é {}'.format(n1,n2,resultado))
